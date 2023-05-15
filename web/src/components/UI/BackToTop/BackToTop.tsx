7ba136c9ee5971c86191ae36b95026a233dbab98
Nikolai Tatianenko <nikolai.tatianenko@gmail.com>
2023-05-15 19:53:59 +0200

    feat: BackToTop custom hook

---
+++ web/src/components/UI/BackToTop/BackToTop.tsx
@@ -0,0 +1,58 @@
+'use client';
+import React, { useEffect, useState } from 'react';
+import useScrollPosition from './hooks/useScrollPosition';
+const BackToTopButton: React.FC = () => {
+  const { showButton, scrollPosition } = useScrollPosition(300);
+
+  const handleBackToTop = () => {
+    window.scrollTo({ top: 0, behavior: 'smooth' });
+  };
+
+  const handleScrollToBottom = () => {
+    const fullHeight = document.documentElement.scrollHeight;
+    window.scrollTo({ top: fullHeight, behavior: 'smooth' });
+  };
+
+  const BackToTop: React.FC<{
+    showButton: boolean;
+    handleBackToTop: () => void;
+    className?: string;
+  }> = ({ showButton, handleBackToTop, className = '' }) => (
+    <button
+      className={`fixed bottom-4 right-4 p-2 rounded-full ${className} ${
+        showButton ? 'block' : 'hidden'
+      }`}
+      onClick={handleBackToTop}
+    >
+      <svg
+        xmlns="http://www.w3.org/2000/svg"
+        fill="none"
+        viewBox="0 0 24 24"
+        stroke="currentColor"
+        className={`h-6 w-6 transform ${
+          scrollPosition > 0 ? 'rotate-180' : 'rotate-0'
+        }`}
+      >
+        <path
+          strokeLinecap="round"
+          strokeLinejoin="round"
+          strokeWidth={2}
+          d="M19 14l-7 7m0 0l-7-7m7 7V3"
+        />
+      </svg>
+    </button>
+  );
+
+  return (
+    <>
+      <BackToTop
+        className="rotate-180"
+        showButton={scrollPosition > 10 && !showButton}
+        handleBackToTop={handleScrollToBottom}
+      />
+      <BackToTop showButton={showButton} handleBackToTop={handleBackToTop} />
+    </>
+  );
+};
+
+export default BackToTopButton;
