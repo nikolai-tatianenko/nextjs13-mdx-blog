48605d16f708caa867381190b18b9626f6a6f4e2
Nikolai Tatianenko <nikolai.tatianenko@gmail.com>
2023-05-16 02:10:59 +0000

    feat: BackToTop Component : Storybook

---
+++ web/src/components/UI/BackToTop/BackTopTop.stories.tsx
@@ -0,0 +1,100 @@
+import { Meta, StoryObj } from '@storybook/react';
+import BackToTop from './BackToTop';
+/**
+ * Component that displays the BackToTopButton storybook.
+ */
+const BackTopTopComp = () => (
+  <>
+    <h1>Welcome to the BackToTopButton storybook!</h1>
+    <br />
+    <p>Scroll down to see the BackToTopButton in action.</p>
+    ⬇<br />
+    ⬇<br />
+    ⬇<br />
+    ⬇<br />
+    ⬇<br />
+    ⬇<br />
+    ⬇<br />
+    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+    ⬇<br />
+    ⬇<br />
+    ⬇<br />
+    ⬇<br />
+    ⬇<br />
+    ⬇<br />
+    ⬇<br />
+    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+    ⬇<br />
+    ⬇<br />
+    ⬇<br />
+    ⬇<br />
+    ⬇<br />
+    ⬇<br />
+    ⬇<br />
+    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+    ⬇<br />
+    ⬇<br />
+    ⬇<br />
+    ⬇<br />
+    ⬇<br />
+    ⬇<br />
+    ⬇<br />
+    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+    ⬇<br />
+    ⬇<br />
+    ⬇<br />
+    ⬇<br />
+    ⬇<br />
+    ⬇<br />
+    ⬇<br />
+    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+    ⬇<br />
+    ⬇<br />
+    ⬇<br />
+    ⬇<br />
+    ⬇<br />
+    ⬇<br />
+    ⬇<br />
+    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+    ⬇<br />
+    ⬇<br />
+    ⬇<br />
+    ⬇<br />
+    ⬇<br />
+    ⬇<br />
+    ⬇<br />
+    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+    ⬇<br />
+    ⬇<br />
+    ⬇<br />
+    ⬇<br />
+    ⬇<br />
+    ⬇<br />
+    ⬇<br />
+    <BackToTop />
+  </>
+);
+
+/**
+ * The meta information for the BackToTopButton storybook.
+ */
+const meta: Meta = {
+  title: 'UI/BackToTop',
+  component: BackTopTopComp,
+  tags: ['autodocs', 'docsPage'],
+};
+
+export default meta;
+
+type Story = StoryObj<typeof BackTopTopComp>;
+
+/**
+ * The primary story for the BackToTopButton.
+ *
+ * More on writing stories with args: https://storybook.js.org/docs/react/writing-stories/args
+ */
+export const Primary: Story = {
+  args: {
+    primary: true,
+  },
+};
