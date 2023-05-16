1f22e1f2227b48a935346b36cc86ae5db6169ba1
Nikolai Tatianenko <nikolai.tatianenko@gmail.com>
2023-05-16 21:00:46 +0000

    feat: TOC Hook Get Headers : Unit Tests

---
+++ web/src/components/UI/TOC/hooks/useHeadings/useHeadings.test.tsx
@@ -0,0 +1,28 @@
+import { renderHook } from '@testing-library/react-hooks';
+import { useHeadings, Heading } from './useHeadings';
+
+describe('useHeadings', () => {
+  test('returns array of headings', () => {
+    const mockHeadings: Heading[] = [
+      { id: 'heading1', title: 'Heading 1', level: 2 },
+      { id: 'heading2', title: 'Heading 2', level: 3 },
+    ];
+
+    jest.spyOn(document, 'querySelectorAll').mockReturnValueOnce([
+      {
+        id: 'heading1',
+        textContent: 'Heading 1',
+        tagName: 'H2',
+      },
+      {
+        id: 'heading2',
+        textContent: 'Heading 2',
+        tagName: 'H3',
+      },
+    ] as any);
+
+    const { result } = renderHook(() => useHeadings());
+
+    expect(result.current).toEqual(mockHeadings);
+  });
+});
