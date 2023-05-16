4cdbb2729456a131418ef0ca1b0aef1ffd27f03a
Nikolai Tatianenko <nikolai.tatianenko@gmail.com>
2023-05-16 23:51:46 +0000

    feat: TOC item Component

--- web/src/components/UI/TOC/TOC.tsx
+++ web/src/components/UI/TOC/TOC.tsx
@@ -17,6 +17,49 @@ type TableOfContentsItemProps = {
  * @returns {JSX.Element} - Rendered component.
  */
 const TableOfContentsItem: React.FC<TableOfContentsItemProps> = React.memo(
+  ({ id, level, title, isActive }) => {
+    const headerClassname =
+      'block border-b-2 p-2 m-2 w-full text-md mb-3 w-[50%] text-center';
+    const focusItemClass = 'font-bold text-gray-900 border-l-gray-900';
+
+    const hoverItemClass = focusItemClass
+      .split(' ')
+      .map((val) => `hover:${val}`)
+      .join(' ');
+
+    const activeItemClass = focusItemClass
+      .split(' ')
+      .map((val) => `active:${val}`)
+      .join(' ');
+
+    const linkClassNames = cx(
+      `level-${level}`,
+      'block border-l-2 border-accent-2 p-2 text-sm transition-all duration-500 focus:outline-none focus:ring hover:bg-gray-200',
+      hoverItemClass,
+      activeItemClass,
+      {
+        [`${focusItemClass}`]: isActive,
+        'active-toc-item': isActive,
+      }
+    );
+
+    const paddingSize = 8;
+    const paddingLeft = (level - 1) * paddingSize;
+
+    if (level === 1) {
+      return (
+        <a href={`#${id}`} className={headerClassname} style={{ paddingLeft }}>
+          {title}
+        </a>
+      );
+    }
+
+    return (
+      <a href={`#${id}`} className={linkClassNames} style={{ paddingLeft }}>
+        {title}
+      </a>
+    );
+  }
 );
 
 /**
