6f2752df11c955c90c69877ade53dbb6fef3db1e
Nikolai Tatianenko <nikolai.tatianenko@gmail.com>
2023-05-16 01:53:59 +0000

    feat: BackToTop Component

--- web/src/components/UI/BackToTop/BackToTop.tsx
+++ web/src/components/UI/BackToTop/BackToTop.tsx
@@ -1,8 +1,21 @@
 'use client';
-import React, { useEffect, useState } from 'react';
+import React from 'react';
 import useScrollPosition from './hooks/useScrollPosition';
-export const BackToTop: React.FC = () => {
-  const { showButton, scrollPosition } = useScrollPosition(300);
+import { BackToTopButton } from './components/BackToTopButton';
+
+/**
+ * Props for the BackToTop component.
+ */
+type BackToTopProps = {
+  threshold?: number;
+};
+
+/**
+ * Component to scroll to the top or bottom of the page.
+ */
+export const BackToTop: React.FC<BackToTopProps> = ({ threshold = 300 }) => {
+  const { showButton: showBackToTopButton, scrollPosition } =
+    useScrollPosition(threshold);
 
   const handleBackToTop = () => {
     window.scrollTo({ top: 0, behavior: 'smooth' });
@@ -13,46 +26,18 @@ export const BackToTop: React.FC = () => {
     window.scrollTo({ top: fullHeight, behavior: 'smooth' });
   };
 
-  const BackToTopButton: React.FC<{
-    showButton: boolean;
-    handleBackToTop: () => void;
-    className?: string;
-  }> = ({ showButton, handleBackToTop, className = '' }) => (
-    <button
-      className={`back2top border-b-gray-200 bg-gray-300 fixed bottom-4 right-4 p-2 rounded-full ${className} ${
-        showButton ? 'block' : 'hidden'
-      }`}
-      onClick={handleBackToTop}
-    >
-      <svg
-        xmlns="http://www.w3.org/2000/svg"
-        fill="none"
-        viewBox="0 0 24 24"
-        stroke="currentColor"
-        className={`h-6 w-6 transform ${
-          scrollPosition > 0 ? 'rotate-180' : 'rotate-0'
-        }`}
-      >
-        <path
-          strokeLinecap="round"
-          strokeLinejoin="round"
-          strokeWidth={2}
-          d="M19 14l-7 7m0 0l-7-7m7 7V3"
-        />
-      </svg>
-    </button>
-  );
-
   return (
     <>
+      {/* Scroll to top */}
       <BackToTopButton
         className="rotate-180"
-        showButton={scrollPosition > 10 && !showButton}
-        handleBackToTop={handleScrollToBottom}
-      />
-      <BackToTopButton
-        showButton={showButton}
+        showButton={showBackToTopButton}
         handleBackToTop={handleBackToTop}
+      />
+      {/* Scroll to bottom */}
+      <BackToTopButton
+        showButton={!showBackToTopButton}
+        handleBackToTop={handleScrollToBottom}
       />
     </>
   );
