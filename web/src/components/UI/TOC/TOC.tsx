4404a1c7d05d179f6e61e2eed333d339c65a7c97
Nikolai Tatianenko <nikolai.tatianenko@gmail.com>
2023-05-16 23:05:46 +0000

    feat: TOC get header iteams and scrollspy

--- web/src/components/UI/TOC/TOC.tsx
+++ web/src/components/UI/TOC/TOC.tsx
@@ -25,6 +25,22 @@ const TableOfContentsItem: React.FC<TableOfContentsItemProps> = React.memo(
  * @returns {JSX.Element} - Rendered component.
  */
 const TableOfContents: React.FC = () => {
+  const headings = useHeadings('article :is(h2, h3, h4, h5, h6)');
+
+  const [activeId, setActiveId] = useState<string | undefined>(headings[0]?.id);
+  const [isCollapsible, setIsCollapsible] = useState(true);
+  const [isCollapsed, setIsCollapsed] = useState(true);
+
+  useEffect(() => {
+    setActiveId(headings[0]?.id);
+  }, [headings]);
+
+  useScrollspy(
+    headings.map((heading) => heading.id),
+    (newActiveId) => {
+      setActiveId(newActiveId);
+    }
+  );
 };
 
 export default TableOfContents;
