f284faae282b134f759ca32dc6c7f7f311d7baac
Nikolai Tatianenko <nikolai.tatianenko@gmail.com>
2023-05-16 05:39:59 +0000

    refactoring: integrate Heading to MDX

--- web/src/components/MdxContentWrapper.tsx
+++ web/src/components/MdxContentWrapper.tsx
@@ -1,6 +1,7 @@
 'use client';
 
 import { MDXRemote, type MDXRemoteSerializeResult } from 'next-mdx-remote';
+import { Heading } from './UI';
 
 /**
  * The properties for the MDX content component.
@@ -12,7 +13,15 @@ type MdxContentProps = {
 /**
  * A mapping of MDX components used for rendering.
  */
-const mdxComponentsMap = {};
+const mdxComponentsMap = {
+  h1: (props: any) => <Heading level={1} {...props} />,
+  h2: (props: any) => <Heading level={2} {...props} />,
+  h3: (props: any) => <Heading level={3} {...props} />,
+  h4: (props: any) => <Heading level={4} {...props} />,
+  h5: (props: any) => <Heading level={5} {...props} />,
+  h6: (props: any) => <Heading level={6} {...props} />,
+  Heading: (props: any) => <Heading {...props} />,
+};
 
 /**
  * Renders the MDX content wrapped in an MDXRemote component.
