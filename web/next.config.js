15c7b5d0403a07c9380d930ab83ea8956ab8752d
Nikolai Tatianenko <nikolai.tatianenko@gmail.com>
2023-05-17 02:06:46 +0000

    feat: temporary disable errors on build

--- web/next.config.js
+++ web/next.config.js
@@ -1,6 +1,14 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: 'export',
+  typescript: {
+    ignoreBuildErrors: true,
+    // ignoreBuildErrors: process.env.ENV === 'production',
+  },
+  eslint: {
+    ignoreDuringBuilds: true,
+    // ignoreDuringBuilds: process.env.ENV === 'production',
+  },
 };
 
 module.exports = nextConfig;
