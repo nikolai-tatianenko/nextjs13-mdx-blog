5d1b98c1e7f2f4a8b2d2ca12719c8d24415c54a1
Nikolai Tatianenko <nikolai.tatianenko@gmail.com>
2023-05-17 00:41:46 +0000

    feat: Header

---
+++ web/src/components/Layouts/Header.stories.js
@@ -0,0 +1,16 @@
+import { Header } from './Header';
+
+const meta = {
+  title: 'Layout/Header',
+  component: Header,
+  // This component will have an automatically generated docsPage entry: https://storybook.js.org/docs/7.0/react/writing-docs/docs-page
+  tags: ['docsPage'],
+  parameters: {
+    // More on how to position stories at: https://storybook.js.org/docs/7.0/react/configure/story-layout
+    layout: 'fullscreen',
+  },
+};
+
+export default meta;
+
+export const Primary = {};
