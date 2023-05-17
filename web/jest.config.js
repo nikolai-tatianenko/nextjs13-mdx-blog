b1b02165561ab93d9a06028d1072abb421764f43
Nikolai Tatianenko <nikolai.tatianenko@gmail.com>
2023-05-17 02:32:46 +0000

    feat: jest initial setup

---
+++ web/jest.config.js
@@ -0,0 +1,8 @@
+module.exports = {
+  testEnvironment: 'jsdom',
+  moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx'],
+  transform: {
+    '^.+\\.(ts|tsx)$': 'ts-jest',
+  },
+  testMatch: ['<rootDir>/src/**/*.test.(ts|tsx)'],
+};
