9bf05fa9880ef7369c82d52b327215171bf51b47
Nikolai Tatianenko <nikolai.tatianenko@gmail.com>
2023-05-25 21:41:46 +0000

    refactor: TOC Hook rewroted

--- web/src/components/03-Organisms/TOC/hooks/useHeadings/useHeadings.tsx
+++ web/src/components/03-Organisms/TOC/hooks/useHeadings/useHeadings.tsx
@@ -10,36 +10,55 @@ type Heading = {
  * Custom hook to retrieve headings from the document based on the given query.
  *
  * @param {string} [headingQuery='article :is(h1, h2, h3, h4, h5, h6)'] - Query selector for headings.
+ * @param {string} pathname - Current pathname.
  * @returns {Heading[]} - Array of headings.
  */
 export const useHeadings = (
-  headingQuery = "article :is(h1, h2, h3, h4, h5, h6)", pathname: string
+  headingQuery = "article :is(h1, h2, h3, h4, h5, h6)",
+  pathname: string
 ): Heading[] => {
   const [headings, setHeadings] = useState<Heading[]>([]);
 
-  const getHeaders = (): void => {
-    const elements = Array.from(
-      document.querySelectorAll<HTMLHeadingElement>(headingQuery)
-    ).reduce<Heading[]>((acc, element) => {
-      if (element.id) {
-        acc.push({
-          id: element.id,
-          title: element.textContent || "",
-          level: Number(element.tagName.substring(1))
-        });
-      }
-      return acc;
-    }, []);
-
-    setHeadings(elements);
-  };
   useEffect(() => {
+    const getHeaders = (): void => {
+      const elements = Array.from(
+        document.querySelectorAll<HTMLHeadingElement>(headingQuery)
+      ).reduce<Heading[]>((acc, element) => {
+        if (element.id) {
+          acc.push({
+            id: element.id,
+            title: element.textContent || "",
+            level: Number(element.tagName.substring(1)),
+          });
+        }
+        return acc;
+      }, []);
+
+      setHeadings(elements);
+    };
+
     getHeaders();
-  }, []);
+  }, [headingQuery]);
 
   useEffect(() => {
-    getHeaders();
+    const getHeaders = (): void => {
+      const elements = Array.from(
+        document.querySelectorAll<HTMLHeadingElement>(headingQuery)
+      ).reduce<Heading[]>((acc, element) => {
+        if (element.id) {
+          acc.push({
+            id: element.id,
+            title: element.textContent || "",
+            level: Number(element.tagName.substring(1)),
+          });
+        }
+        return acc;
+      }, []);
 
+      setHeadings(elements);
+    };
+
+    getHeaders();
   }, [pathname]);
 
   return headings;
