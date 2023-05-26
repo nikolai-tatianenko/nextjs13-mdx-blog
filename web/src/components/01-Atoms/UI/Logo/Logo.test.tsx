a5a21342435c4e0a533b54ed516e61fb9f0d196f
Nikolai Tatianenko <nikolai.tatianenko@gmail.com>
2023-05-26 04:23:46 +0000

    test: Logo with params

--- web/src/components/01-Atoms/UI/Logo/Logo.test.tsx
+++ web/src/components/01-Atoms/UI/Logo/Logo.test.tsx
@@ -1,13 +1,25 @@
 import { render, screen } from "@testing-library/react";
-import Logo, { LogoProps } from "./Logo";
+import Logo from "./Logo";
 
 describe("Logo", () => {
-  test("renders the logo component with the default sitename if not specified", () => {
-    render(<Logo />);
+  test("renders the logo component with the default sitename if not specified",
+    () => {
+      render(<Logo />);
+      const linkElement = screen.getByRole("link");
+      expect(linkElement).toBeInTheDocument();
+      expect(linkElement).toHaveAttribute("href", "/");
+      const strongElement = screen.getByText("Site Name");
+      expect(strongElement).toBeInTheDocument();
+    });
+
+  test("renders the logo component with the specified sitename", () => {
+    const sitename = "My Site";
+    render(<Logo sitename={sitename} />);
     const linkElement = screen.getByRole("link");
     expect(linkElement).toBeInTheDocument();
     expect(linkElement).toHaveAttribute("href", "/");
-    const strongElement = screen.getByText("Site Name");
+    const strongElement = screen.getByText(sitename);
     expect(strongElement).toBeInTheDocument();
   });
+
 });
