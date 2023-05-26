253977306800a0463fa995c08df0d2061abed6ea
Nikolai Tatianenko <nikolai.tatianenko@gmail.com>
2023-05-26 02:39:46 +0000

    feat: Extract Logo Editable text

--- web/src/components/01-Atoms/UI/Logo/Logo.tsx
+++ web/src/components/01-Atoms/UI/Logo/Logo.tsx
@@ -1,16 +1,22 @@
+import Link from "next/link";
+import React from "react";
+
+export type LogoProps = {
+  sitename?: string;
+}
+
 /**
  * Renders the logo component with a link to the homepage.
  *
  * @function Logo
  * @returns {JSX.Element} The logo component.
  */
-import Link from "next/link";
-import React from "react";
-
-export const Logo = () => {
+const Logo: React.FC<LogoProps> = ({ sitename = "Site Name" }) => {
   return (
-    <Link href={"/"}>
-      <strong className={"text-black dark:text-white"}>SiteName</strong>
+    <Link href="/">
+      <strong className="text-black dark:text-white">{sitename}</strong>
     </Link>
   );
 };
+
+export default Logo;
