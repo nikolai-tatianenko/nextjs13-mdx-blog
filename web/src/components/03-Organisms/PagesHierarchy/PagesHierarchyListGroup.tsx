f2a24a35e93f24b4ae99face25ce4e0ca6536f05
Nikolai Tatianenko <nikolai.tatianenko@gmail.com>
2023-05-31 22:01:08 +0000

    feat: Component Pages Hierarchy List

---
+++ web/src/components/03-Organisms/PagesHierarchy/PagesHierarchyListGroup.tsx
@@ -0,0 +1,24 @@
+import React from "react";
+import { PagesHierarchyList } from "./components";
+import { PagesHierarchyListGroupProps } from "./types/listLinkType";
+import { defaultSectionList } from "./data/defaultSectionListItems";
+
+export const PagesHierarchyListGroup: React.FC<PagesHierarchyListGroupProps> = ({
+  sectionList = defaultSectionList
+}) => {
+  return (
+    <nav
+      id="nav"
+      className="sticky?lg:h-(screen-18) px-1 pb-10 pl-3 pt-16 text-base font-normal lg:pb-20 lg:pl-0 lg:pt-2 lg:text-sm"
+      aria-label="Docs navigation"
+    >
+      <ul className="list-unstyled mb-0">
+        {sectionList.map((sectionProps, index) => (
+          <li key={index} className="mt-8">
+            <PagesHierarchyList sectionProps={sectionProps} />
+          </li>
+        ))}
+      </ul>
+    </nav>
+  );
+};
