a53e5536f47fde7c5386235ce723ba78814e8acf
Nikolai Tatianenko <nikolai.tatianenko@gmail.com>
2023-06-19 01:24:51 +0200

    feat: pages list

--- web/src/app/post/page.tsx
+++ web/src/app/post/page.tsx
@@ -1,24 +1,35 @@
-import { HorizontalLine, MdxContentWrapper } from '@/components';
-import TagsList from '@/components/02-Molecules/TagsList';
-import { getAllPages } from '@/lib/mdx/getPage';
-import Link from 'next/link';
+import { Heading, HorizontalLine, MdxContentWrapper } from "@/components";
+import TagsList from "@/components/02-Molecules/TagsList/TagsList";
+import { getAllPages } from "@/lib/mdx/getPage";
+import Link from "next/link";
+import { EmptyComponent } from "@/components/01-Atoms/UI/EmptyComponent";
 
 /**
  * Renders the dynamic posts list page.
  * @constructor
  */
-const PostList: () => Promise<JSX.Element> = async () => {
-  const pagesContent = await getAllPages('post');
+const PostList = async (): Promise<JSX.Element> => {
+  const pagesContent = await getAllPages("post");
+
 
   console.log(pagesContent);
-  return pagesContent.map((pageContent, key) => (
-    <Link href={`${pageContent.type}/${pageContent.file}`}>
-      <MdxContentWrapper source={pageContent.serializedTrimmed} />
-      {/*{pageContent.frontmatter.tags.join(',')}*/}
-      <TagsList tagsList={pageContent.frontmatter.tags} />
-      <HorizontalLine />
-    </Link>
-  ));
+  return (
+    <>
+      {pagesContent.map((pageContent, key) => (
+        <Link href={`${pageContent.type}/${pageContent.file}`} key={key}>
+          <div>
+            <Heading level={2}>{pageContent.frontmatter.title}</Heading>
+            <MdxContentWrapper
+              source={pageContent.serializedTrimmed}
+              hideHeaders={true}
+            />
+            <TagsList tagsList={pageContent.frontmatter.tags} />
+            <HorizontalLine />
+          </div>
+        </Link>
+      ))}
+    </>
+  );
 };
 
 export default PostList;
