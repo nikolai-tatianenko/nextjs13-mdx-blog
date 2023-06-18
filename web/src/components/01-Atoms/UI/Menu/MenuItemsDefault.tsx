f6720d8a61af31240d403e10b140dc084533b091
Nikolai Tatianenko <nikolai.tatianenko@gmail.com>
2023-06-19 00:18:50 +0200

    refactor: added pages

--- web/src/components/01-Atoms/UI/Menu/MenuItemsDefault.tsx
+++ web/src/components/01-Atoms/UI/Menu/MenuItemsDefault.tsx
@@ -2,5 +2,7 @@ import { MenuItems } from './types';
 export const menuItemsDefault: MenuItems[] = [
   { path: '/', label: 'Home' },
   { path: '/about', label: 'About' },
+  { path: '/post', label: 'Posts' },
+  { path: '/tags', label: 'Tags' },
   { path: '/contact', label: 'Contact' },
 ];
