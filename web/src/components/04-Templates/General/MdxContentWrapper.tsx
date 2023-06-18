03da73c8ef2ff8193037ada584606563758b4c5b
Nikolai Tatianenko <nikolai.tatianenko@gmail.com>
2023-06-19 01:26:21 +0200

    feat: hide headers for trimmed content

--- web/src/components/04-Templates/General/MdxContentWrapper.tsx
+++ web/src/components/04-Templates/General/MdxContentWrapper.tsx
@@ -1,5 +1,6 @@
 'use client';
 
+import { EmptyComponent } from '@/components/01-Atoms/UI/EmptyComponent';
 import { MDXRemote, type MDXRemoteSerializeResult } from 'next-mdx-remote';
 import { useState } from 'react';
 import { Heading } from '../../01-Atoms/UI';
@@ -10,12 +11,13 @@ import { Heading } from '../../01-Atoms/UI';
 type MdxContentProps = {
   source: MDXRemoteSerializeResult;
   maxLength?: number;
+  hideHeaders?: boolean;
 };
 
 /**
  * A mapping of MDX components used for rendering.
  */
-const mdxComponentsMap = {
+const mdxComponentsMapDefault = {
   h1: (props: any) => <Heading level={1} {...props} />,
   h2: (props: any) => <Heading level={2} {...props} />,
   h3: (props: any) => <Heading level={3} {...props} />,
@@ -34,16 +36,32 @@ const mdxComponentsMap = {
 export const MdxContentWrapper: React.FC<MdxContentProps> = ({
   source,
   maxLength = 200,
+  hideHeaders = false,
 }) => {
-  const [trimmedContent, setTrimmedContent] = useState<string | null>(null);
+  const [trimmedContent, setTrimmedContent] = useState<boolean>(true);
 
   const handleClickReadMore = () => {
-    setTrimmedContent(null);
+    setTrimmedContent(false);
   };
 
-  // @ts-ignore
-  if (maxLength && !trimmedContent && source.length > maxLength) {
-    // @ts-ignore
+  let mdxComponentsMap = { ...mdxComponentsMapDefault };
+
+  /**
+   * Hide Headers for trimmed content.
+   */
+  if (hideHeaders) {
+    mdxComponentsMap = {
+      ...mdxComponentsMapDefault,
+      h1: EmptyComponent,
+      h2: EmptyComponent,
+      h3: EmptyComponent,
+      h4: EmptyComponent,
+      h5: EmptyComponent,
+      h6: EmptyComponent,
+    };
+  }
+
+  if (maxLength && trimmedContent && source.length > maxLength) {
     const trimmedText = source.slice(0, maxLength);
     const lastSpaceIndex = trimmedText.lastIndexOf(' ');
     const trimmedContentWithEllipsis =
@@ -65,7 +83,7 @@ export const MdxContentWrapper: React.FC<MdxContentProps> = ({
   return (
     <>
       <MDXRemote {...source} components={mdxComponentsMap} />
-      {trimmedContent && (
+      {maxLength && !trimmedContent && (
         <button
           onClick={handleClickReadMore}
           className="text-blue-500 hover:underline"
