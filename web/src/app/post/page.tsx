0428b8fdfb86c16de778bd279d968df875da5241
Nikolai Tatianenko <nikolai.tatianenko@gmail.com>
2023-06-19 00:47:50 +0200

    feat: show trimmed posts data

--- web/src/app/post/page.tsx
+++ web/src/app/post/page.tsx
@@ -1,10 +1,25 @@
 import Page from '@/components/04-Templates/Layouts/Page';
 import { getAllPages } from '@/lib/mdx/getPage';
+import { HorizontalLine, MdxContentWrapper } from "@/components";
+import Link from "next/link";
+import TagsList from "@/components/02-Molecules/TagsList";
 
+/**
+ * Renders the dynamic posts list page.
+ * @constructor
+ */
 const PostList: () => Promise<JSX.Element> = async () => {
   const pagesContent = await getAllPages('post');
 
-  return  pagesContent.map(pageContent => (<Page pageContent={pageContent} />))
+  console.log(pagesContent);
+  return pagesContent.map((pageContent, key) => (
+    <Link href={`${pageContent.type}/${pageContent.file}`}>
+      <MdxContentWrapper source={pageContent.serializedTrimmed} />
+      {/*{pageContent.frontmatter.tags.join(',')}*/}
+      <TagsList tagsList={pageContent.frontmatter.tags} />
+      <HorizontalLine/>
+    </Link>
+  ));
 };
 
 export default PostList;
